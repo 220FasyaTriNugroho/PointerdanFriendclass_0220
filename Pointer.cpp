6b00621ec724e63b3e0ca5e08aee15860fda5e25
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-05-05 18:32:16 +0700

    membuat dereference

--- Pointer.cpp
+++ Pointer.cpp
@@ -11,5 +11,23 @@ using namespace std;
 
 int main()
 {
+    //dereference
+    int a = 10;
+    int b = 6;
+    int y = 20;
+    int *c = &a;
+    cout << "Alamat Memori : " << c << endl;
+    cout << "Nilai dari C : " << *c << endl;
+    c = &y;
+    cout << "Alamat Memori : " << c << endl;
+    cout << "Nilai dari C : " << *c << endl;
 
+
+
+
+
+
+
+
+    
 }
